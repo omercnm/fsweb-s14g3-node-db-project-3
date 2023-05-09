0f658b30e2c793128c0465ebf4bd6c39c4252768
ömer <omercnm23@gmail.com>
2023-05-09 16:26:05 +0300

    tamamlandı.

--- queries.sql
+++ queries.sql
@@ -2,8 +2,15 @@
 
 -- Tüm ürünler(product) için veritabanındaki ProductName ve CategoryName'i listeleyin. (77 kayıt göstermeli)
 
+SELECT ProductName, CategoryName FROM Products as p
+JOIN Categories as c ON c.CategoryID = p.CategoryID
+
 -- 9 Ağustos 2012 öncesi verilmiş tüm siparişleri(order) için sipariş id'si (Id) ve gönderici şirket adını(CompanyName)'i listeleyin. (429 kayıt göstermeli)
 
 -- Id'si 10251 olan siparişte verilen tüm ürünlerin(product) sayısını ve adını listeleyin. ProdcutName'e göre sıralayın. (3 kayıt göstermeli)
+
+SELECT COUNT(ProductName), ProductName FROM OrderDetails
+JOIN Products on OrderDetails.ProductId = Products.ProductId
+where OrderId = 10251 group by ProductName 
 
 -- Her sipariş için OrderId, Müşteri'nin adını(Company Name) ve çalışanın soyadını(employee's LastName). Her sütun başlığı doğru bir şekilde isimlendirilmeli. (16.789 kayıt göstermeli)
